f2fc56aeb626ab8c57ed356fbdd83a5e6e787075
Alex Cline <rev.a.r.cline@gmail.com>
2020-12-13 12:29:36 -0600

    commit tree 8501ef75bf861f2297eef15010d0608c0e92ea61

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
-# Add your code here
+# self tracking dog
+class Dog
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.print_all
+    @@all.each { |dog| puts dog.name }
+  end
+
+end
